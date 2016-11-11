eb3b7c1562d096a5bea680f42f04dadd75183a78
Jorge Avila <Jorge Avila>
2016-11-10 16:30:44 -0800

    Jorge's commit to the Chatbot exercise

--- ChatBot/Bot/views.py
+++ ChatBot/Bot/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("My name is Atl and I am the Code Project team lead. ")
+
+def jorge(request):
+    return HttpResponse("My name is Jorge and I am a Code Project team member.")
--- ChatBot/ChatBot/urls.py
+++ ChatBot/ChatBot/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^bot/jorge/', include('Bot.urls')),
     url(r'^bot/', include('Bot.urls')),
     url(r'^admin/', admin.site.urls),
 ]
